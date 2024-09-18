a4d4d51c90d7a64d9efd3565f2adb06173af37e1
andrei.kernazhytski <a_ker@list.ru>
2024-09-18 09:33:40 +0300

    migration edit

--- modules/prisma/prisma/migrations/20240916061055_init/migration.sql
+++ modules/prisma/prisma/migrations/20240916061055_init/migration.sql
@@ -1,33 +1,33 @@
 -- CreateTable
-CREATE TABLE "Roles" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
+CREATE TABLE Roles (
+    id SERIAL NOT NULL,
+    name TEXT NOT NULL,
 
-    CONSTRAINT "Roles_pkey" PRIMARY KEY ("id")
+    CONSTRAINT Roles_pkey PRIMARY KEY (id)
 );
 
 -- CreateTable
-CREATE TABLE "Users" (
-    "id" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
+CREATE TABLE Users (
+    id TEXT NOT NULL,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL,
 
-    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT Users_pkey PRIMARY KEY (id)
 );
 
 -- CreateTable
-CREATE TABLE "UserRoles" (
-    "usersId" TEXT NOT NULL,
-    "rolesId" INTEGER NOT NULL,
+CREATE TABLE UserRoles (
+    usersId TEXT NOT NULL,
+    rolesId INTEGER NOT NULL,
 
-    CONSTRAINT "UserRoles_pkey" PRIMARY KEY ("usersId","rolesId")
+    CONSTRAINT UserRoles_pkey PRIMARY KEY (usersId,rolesId)
 );
 
 -- AddForeignKey
-ALTER TABLE "UserRoles" ADD CONSTRAINT "UserRoles_usersId_fkey" FOREIGN KEY ("usersId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE UserRoles ADD CONSTRAINT UserRoles_usersId_fkey FOREIGN KEY (usersId) REFERENCES Users(id) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "UserRoles" ADD CONSTRAINT "UserRoles_rolesId_fkey" FOREIGN KEY ("rolesId") REFERENCES "Roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE UserRoles ADD CONSTRAINT UserRoles_rolesId_fkey FOREIGN KEY (rolesId) REFERENCES Roles(id) ON DELETE RESTRICT ON UPDATE CASCADE;
 
-INSERT INTO "Roles" (name) VALUES ('ADMIN'), ('USER');
+INSERT INTO Roles (name) VALUES ('ADMIN'), ('USER');
