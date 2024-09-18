57a2c0b46a77be9b741805cf16ac4fd4a0bae5b9
andrei.kernazhytski <a_ker@list.ru>
2024-09-18 14:52:48 +0300

    VLB-22 products + product types

---
+++ modules/prisma/prisma/migrations/20240918144100_products/migration.sql
@@ -0,0 +1,33 @@
+CREATE TABLE nsi_product_type
+(
+    id   INTEGER      NOT NULL,
+    name VARCHAR(255) NOT NULL,
+
+    CONSTRAINT nsi_product_type_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE products
+(
+    id              INTEGER        NOT NULL,
+    header          VARCHAR(255)   NOT NULL,
+    description     VARCHAR(5000)  NOT NULL,
+    price           NUMERIC(18, 2) NOT NULL,
+
+    owner_id        UUID           NOT NULL,
+
+    product_type_id INTEGER        NOT NULL,
+
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+);
+
+ALTER TABLE products
+    ADD CONSTRAINT products_product_type_id_fkey FOREIGN KEY (product_type_id) REFERENCES nsi_product_type (id);
+
+ALTER TABLE products
+    ADD CONSTRAINT products_owner_id_fkey FOREIGN KEY (owner_id) REFERENCES users (id);
+
+INSERT INTO nsi_product_type (id, name)
+VALUES (1, 'Лабораторная работа'),
+       (2, 'Курсовая работа'),
+       (3, 'Дипломная работа'),
+       (4, 'Прочие работы');
