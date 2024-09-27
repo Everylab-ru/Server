7717a834bbda7e14181ace75751758193cbd759c
andrei.kernazhytski <a_ker@list.ru>
2024-09-27 10:24:49 +0300

    VLB-33 Files DB

---
+++ modules/prisma/prisma/migrations/20240927101600_files/migration.sql
@@ -0,0 +1,27 @@
+CREATE TABLE file_types
+(
+    id   INTEGER      NOT NULL,
+    name VARCHAR(255) NOT NULL,
+
+    CONSTRAINT nsi_product_type_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE files
+(
+    id           INTEGER      NOT NULL,
+    url          UUID         NOT NULL,
+    user_id      UUID,
+    file_type_id INTEGER      NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    format       VARCHAR(255) NOT NULL,
+
+    CONSTRAINT files_pkey PRIMARY KEY (id),
+
+    CONSTRAINT files_file_type_id_fkey FOREIGN KEY (file_type_id) REFERENCES file_types (id),
+    CONSTRAINT files_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+INSERT INTO file_types (id, name)
+VALUES (1, 'Курсовая работа'),
+       (2, 'Лабораторная работа'),
+       (3, 'Дипломная работа');
